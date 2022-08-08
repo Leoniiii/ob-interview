39dbfe21b039c8cd9e30b7b184448897495e0430
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2022-08-08 19:44:50 +0200

    Finish header layout and create header module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventsComponent } from './components/events/events.component';
 import { EventCardComponent } from './components/event-card/event-card.component';
-import { HeaderComponent } from './components/header/header.component';
 import { SessionsComponent } from './components/sessions/sessions.component';
 import { SessionsListComponent } from './components/sessions-list/sessions-list.component';
 import { CartComponent } from './components/cart/cart.component';
+import { HeaderModule } from './components/header/header.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventsComponent,
     EventCardComponent,
-    HeaderComponent,
     SessionsComponent,
     SessionsListComponent,
     CartComponent
   ],
   imports: [
+    HeaderModule,
     BrowserModule,
     AppRoutingModule
   ],
