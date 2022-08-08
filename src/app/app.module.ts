1114c36549026d4613cfbec2ce0d2248aca88cad
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2022-08-08 19:27:42 +0200

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventsComponent } from './components/events/events.component';
+import { EventCardComponent } from './components/event-card/event-card.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SessionsComponent } from './components/sessions/sessions.component';
+import { SessionsListComponent } from './components/sessions-list/sessions-list.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsComponent,
+    EventCardComponent,
+    HeaderComponent,
+    SessionsComponent,
+    SessionsListComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
