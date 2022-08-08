a946a234cdac4f74c3b34716f68df82915eebc74
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2022-08-08 21:00:25 +0200

    Working in sessions layout

--- src/app/components/sessions/sessions.component.ts
+++ src/app/components/sessions/sessions.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sessions',
   templateUrl: './sessions.component.html',
-  styleUrls: ['./sessions.component.css']
 })
 export class SessionsComponent implements OnInit {
 
--- src/app/components/sessions/sessions.module.ts
+++ src/app/components/sessions/sessions.module.ts
@@ -1,8 +1,15 @@
+import { CartModule } from './../cart/cart.module';
+import { SessionsListModule } from './../sessions-list/sessions-list.module';
+import { Routes, RouterModule } from '@angular/router';
 import { SessionsComponent } from './sessions.component';
 import { NgModule } from "@angular/core";
 
+const routes: Routes = [
+    { path: '', component: SessionsComponent }
+]
 @NgModule({
     declarations: [SessionsComponent],
-    exports: [SessionsComponent]
+    exports: [SessionsComponent],
+    imports: [RouterModule.forChild(routes), SessionsListModule, CartModule]
 })
 export class SessionsModule { }
