aad1dbd43eda8cb61d58025b3ed411325b3e2e93
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2022-08-09 23:11:31 +0200

    Delete unused files

--- src/app/components/events/events.component.ts
+++ src/app/components/events/events.component.ts
@@ -3,7 +3,6 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.css'],
 })
 export class EventsComponent implements OnInit {
 
--- src/app/components/sessions-list/sessions-list.component.ts
+++ src/app/components/sessions-list/sessions-list.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sessions-list',
   templateUrl: './sessions-list.component.html',
-  styleUrls: ['./sessions-list.component.css']
 })
 export class SessionsListComponent implements OnInit {
 
